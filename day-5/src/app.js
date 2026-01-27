2eac969bb438acd1c55e361b814907bbed96526c
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-01-28 03:12:11 +0530

    day 5

---
+++ day-5/src/app.js
@@ -0,0 +1,46 @@
+const express =require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+// Post /notes \\
+app.post("/notes",(req,res)=>{
+    
+    notes.push(req.body)
+
+    res.status(201).json({
+        message: "Note created successfully"
+    })
+})
+
+// Get /notes \\
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+        notes: notes
+    })
+})
+
+// delete /notes/:index \\
+app.delete("/notes/:index",(req,res)=>{
+    delete notes[req.params.index]
+
+    res.status(204).json({
+        message: "Note deleted successfully"
+    })
+})  
+
+// Patch /notes/:index \\
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].description = req.body.description
+
+    res.status(200).json({
+        message: "Note updated successfully"
+    })
+
+})
+
+
+module.exports = app
