a604e3f391695a15e39be16dcb4de3b1028a9da0
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-02-12 02:42:14 +0530

    fix url

--- day-9/Backend/src/app.js
+++ day-9/Backend/src/app.js
@@ -7,7 +7,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static("./public"))
+app.use(express.static(path.join(__dirname, "../public")))
+
 
 /* Post/api/notes
     - create new note and save data to mongodb 
@@ -71,12 +72,5 @@ app.patch('/api/notes/:id', async (req,res)=>{
         message:"Note update successfully"
     })
 })
-
-console.log(__dirname)
-
-app.use('*name',(req,res)=>{
-    res.sendFile(path.join(__dirname, "..", "/public/index.html"))
-})
-
 
 module.exports = app
