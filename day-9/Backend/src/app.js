1f12e3732fda3b78a755d8595810cc959453e02d
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-02-12 03:50:45 +0530

    fix

--- day-9/Backend/src/app.js
+++ day-9/Backend/src/app.js
@@ -7,9 +7,6 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "../public")))
-
-
 
 
 /* Post/api/notes
@@ -73,10 +70,6 @@ app.patch('/api/notes/:id', async (req,res)=>{
     res.status(200).json({
         message:"Note update successfully"
     })
-})
-
-app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 
 module.exports = app    
